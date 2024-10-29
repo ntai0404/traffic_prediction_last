01481517aaa2768e6e146baa0649e169408b2063
Nguyen Xuan Tai <ntai04042004@gmail.com>
2024-10-29 11:05:49 +0700

    Cập nhật mã nguồn cho dự án dự đoán giao thông

--- data/data1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('./data/dataset.csv')
-
-count_series = df['Traffic Situation'].value_counts()
-#count_series = df['Date'].value_counts()
-print(count_series)
